8013f8f2a0ecfcabdda412a18768b039ebb2d9c4
raquelmuniz <munizraquel70@gmail.com>
2022-06-14 21:55:41 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+for (let num1 = 1; num1 <= 10; num1++);
+console.log(num1);
+
+let num2 = 10;
+while(num2 >= 1){
+    console.log(num2);
+    num2--;
+}
+
+for (let num3 = 1; num3 <=100; num3+=2);
+console.log(num3);
+
+for (let num4 = 0; num4 <= 100; num4 += 2);
+console.log(num4);
